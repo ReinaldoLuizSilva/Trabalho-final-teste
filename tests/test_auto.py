286481513c2dcd050d12f3a2186cf1df433f7ee8
ReinaldoLuizSilva <reinaldodasilva@ienh.com.br>
2024-11-24 18:28:46 -0300

    teste39

--- tests/test_auto.py
+++ tests/test_auto.py
@@ -171,3 +171,26 @@ def test_deletar_funcionario():
         WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//button[text()=' Yes, Delete ']"))).click()
     finally:
         driver.quit()
+
+def test_busca_funcionario():
+    selenium_url = os.getenv("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
+    driver = webdriver.Remote(
+        command_executor=selenium_url,
+        options=webdriver.FirefoxOptions()
+    )
+
+    try:
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+
+
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("Admin")
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys("admin123")
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME, "orangehrm-login-button"))).click()
+
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(@class, 'oxd-main-menu-item--name') and text()='PIM']"))).click()
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'oxd-autocomplete-text-input oxd-autocomplete-text-input--active')]"))).send_keys("Peter Mac")
+
+        WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, "//span[contains(@class, 'oxd-text oxd-text--span')and text()='(1) Record Found']")))
+    
+    finally:
+        driver.quit()
