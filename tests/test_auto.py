d3cf800068c74ab1cabc136d6826c19ca1640264
ReinaldoLuizSilva <reinaldodasilva@ienh.com.br>
2024-11-24 20:13:20 -0300

    teste51

--- tests/test_auto.py
+++ tests/test_auto.py
@@ -263,10 +263,33 @@ def test_criar_recruitment():
 
         WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "firstName"))).send_keys("Francisco")
         WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "lastName"))).send_keys("Beleza")
-        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, "//input[@data-v-1f99f73c"))).send_keys("FranciscoBeleza@email.com")
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, "//input[@data-v-1f99f73c=''"))).send_keys("FranciscoBeleza@email.com")
 
         WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//button[text()=' Save ']"))).click()
 
         WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, "//h6[contains(@class, 'oxd-text oxd-text--h6 orangehrm-main-title')]")))
+    finally:
+        driver.quit()
+
+def criar_job():
+    selenium_url = os.getenv("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
+    driver = webdriver.Remote(
+        command_executor=selenium_url,
+        options=webdriver.FirefoxOptions()
+    )
+
+    try:
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+
+
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("Admin")
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys("admin123")
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME, "orangehrm-login-button"))).click()
+
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(@class, 'oxd-main-menu-item--name') and text()='Admin']"))).click()
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME, "--active oxd-topbar-body-nav-tab --parent"))).click()
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(@class, 'oxd-topbar-body-nav-tab-link')and text()='Job Titles']"))).click()
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'oxd-button--secondary') and text()=' Add ']"))).click()
+
     finally:
         driver.quit()
