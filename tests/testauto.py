d7944bef1fdb7f428e28feab72eaf10f1f1089a3
ReinaldoLuizSilva <reinaldodasilva@ienh.com.br>
2024-11-24 15:50:08 -0300

    teste11

--- tests/testauto.py
+++ tests/testauto.py
@@ -1,27 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
 
 def test_login_success():
-    driver = webdriver.Firefox()
-    driver.get("https://opensource-demo.orangehrmlive.com/")
+    # Conecta ao Selenium Remote WebDriver
+    selenium_url = os.getenv("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
+    driver = webdriver.Remote(
+        command_executor=selenium_url,
+        options=webdriver.FirefoxOptions()
+    )
 
-    time.sleep(2)
+    try:
+        # Acessa o site
+        driver.get("https://opensource-demo.orangehrmlive.com/")
 
-    driver.find_element(By.NAME, "username").send_keys("Admin")
+        # Espera até que o campo de usuário esteja presente e interage com ele
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("Admin")
 
-    time.sleep(2)
+        # Interage com o campo de senha
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys("admin123")
 
-    driver.find_element(By.NAME, "password").send_keys("admin123")
+        # Clica no botão de login
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "orangehrm-login-button"))).click()
 
-    time.sleep(2)
+        # Confirma se o redirecionamento para o dashboard foi feito
+        WebDriverWait(driver, 10).until(EC.url_contains("dashboard"))
+        assert "dashboard" in driver.current_url
 
-    driver.find_element(By.CLASS_NAME, "orangehrm-login-button").click()
-
-    time.sleep(2)
-
-    assert "dashboard" in driver.current_url
-    
-    driver.quit()
-test_login_success()
+    finally:
+        driver.quit()
