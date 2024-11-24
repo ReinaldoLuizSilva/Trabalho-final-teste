0a20f37036d5af3c26942df0d6a55d4370898b04
ReinaldoLuizSilva <reinaldodasilva@ienh.com.br>
2024-11-24 16:20:11 -0300

    teste17

--- tests/test_auto.py
+++ tests/test_auto.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 
 def test_login_success():
-    # Conecta ao Selenium Remote WebDriver
     selenium_url = os.getenv("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
     driver = webdriver.Remote(
         command_executor=selenium_url,
@@ -13,17 +12,44 @@ def test_login_success():
     )
 
     try:
-        # Acessa o site
         driver.get("https://opensource-demo.orangehrmlive.com/")
 
-        # Localizar e interagir com os campos de login e o botão
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("Admin")
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys("admin123")
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "orangehrm-login-button"))).click()
 
-        # Verificar se o login foi bem-sucedido
-        WebDriverWait(driver, 10).until(EC.url_contains("dashboard"))
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("Admin")
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys("admin123")
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME, "orangehrm-login-button"))).click()
+
+        WebDriverWait(driver, 2).until(EC.url_contains("dashboard"))
         assert "dashboard" in driver.current_url.lower()
 
     finally:
         driver.quit()
+
+def test_add_employee():
+    selenium_url = os.getenv("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
+    driver = webdriver.Remote(
+        command_executor=selenium_url,
+        options=webdriver.FirefoxOptions()
+    )
+    
+    try:
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+
+
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("Admin")
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys("admin123")
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME, "orangehrm-login-button"))).click()
+
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='PIM']"))).click()
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Add ']"))).click()
+    
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "firstName"))).send_keys("Francisco")
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "lastName"))).send_keys("Beleza")
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//button[text()=' Save ']"))).click()
+    
+        WebDriverWait(driver, 10).until(EC.url_contains("viewEmployeeList"))
+        assert "viewEmployeeList" in driver.current_url.lower()
+
+    finally:
+        driver.quit()
+    
