69ad2ad69134798ec4664a3143323089c3cd400d
ReinaldoLuizSilva <reinaldodasilva@ienh.com.br>
2024-11-24 16:23:46 -0300

    teste18

--- tests/test_auto.py
+++ tests/test_auto.py
@@ -35,18 +35,21 @@ def test_add_employee():
     try:
         driver.get("https://opensource-demo.orangehrmlive.com/")
 
+        # Login
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("Admin")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys("admin123")
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "orangehrm-login-button"))).click()
 
-        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("Admin")
-        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys("admin123")
-        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME, "orangehrm-login-button"))).click()
-
-        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='PIM']"))).click()
-        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Add ']"))).click()
+        # Navegar para PIM e clicar em "Add"
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'PIM')]"))).click()
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'Add')]"))).click()
     
-        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "firstName"))).send_keys("Francisco")
-        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "lastName"))).send_keys("Beleza")
-        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//button[text()=' Save ']"))).click()
+        # Preencher o formulário de adição de funcionário
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "firstName"))).send_keys("Francisco")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "lastName"))).send_keys("Beleza")
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[text()=' Save ']"))).click()
     
+        # Verificar se foi redirecionado para a lista de funcionários
         WebDriverWait(driver, 10).until(EC.url_contains("viewEmployeeList"))
         assert "viewEmployeeList" in driver.current_url.lower()
 
