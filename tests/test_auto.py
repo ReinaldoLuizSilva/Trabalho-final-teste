e7bb114061d583cdc78bc8929a56ac45372862b5
ReinaldoLuizSilva <reinaldodasilva@ienh.com.br>
2024-11-24 16:02:30 -0300

    teste16

--- tests/test_auto.py
+++ tests/test_auto.py
@@ -1,30 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
 
 def test_login_success():
-    driver = webdriver.Firefox()
-    driver.get("https://opensource-demo.orangehrmlive.com/")
+    # Conecta ao Selenium Remote WebDriver
+    selenium_url = os.getenv("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
+    driver = webdriver.Remote(
+        command_executor=selenium_url,
+        options=webdriver.FirefoxOptions()
+    )
 
-    time.sleep(2)
+    try:
+        # Acessa o site
+        driver.get("https://opensource-demo.orangehrmlive.com/")
 
-    # Localizar o campo de usuário e senha e o botão de login usando a classe dos elementos
-    driver.find_element(By.NAME, "username").send_keys("Admin")
+        # Localizar e interagir com os campos de login e o botão
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("Admin")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys("admin123")
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "orangehrm-login-button"))).click()
 
-    time.sleep(2)
+        # Verificar se o login foi bem-sucedido
+        WebDriverWait(driver, 10).until(EC.url_contains("dashboard"))
+        assert "dashboard" in driver.current_url.lower()
 
-    driver.find_element(By.NAME, "password").send_keys("admin123")
-
-    time.sleep(2)
-
-    driver.find_element(By.CLASS_NAME, "orangehrm-login-button").click()
-
-    # Aguardar o carregamento da página após o login
-    time.sleep(2)
-
-    # Verificar se o login foi bem-sucedido
-    assert "dashboard" in driver.current_url.lower()
-    
-    driver.quit()
-test_login_success()
+    finally:
+        driver.quit()
