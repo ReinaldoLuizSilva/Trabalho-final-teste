c223042f2d9ae0e5c1d3f9a9c8b081d9ab8aed59
ReinaldoLuizSilva <reinaldodasilva@ienh.com.br>
2024-11-24 19:50:21 -0300

    teste48

--- tests/test_auto.py
+++ tests/test_auto.py
@@ -235,7 +235,7 @@ def test_busca_nav():
         WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys("admin123")
         WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME, "orangehrm-login-button"))).click()
     
-        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'oxd-main-menu-search')"))).send_keys("My Info")
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, "//input[contains(@class, 'oxd-input oxd-input--active')"))).send_keys("My Info")
         WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(@class, 'oxd-text oxd-text--span oxd-main-menu-item--name') and text()='My Info']"))).click()
 
         WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, "//h6[contains(@class, 'oxd-text oxd-text--h6 orangehrm-main-title')]")))
