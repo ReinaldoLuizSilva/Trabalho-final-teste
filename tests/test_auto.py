3f661897c41662f72defcfef9ca9a539294ee621
ReinaldoLuizSilva <reinaldodasilva@ienh.com.br>
2024-11-24 22:30:34 -0300

    teste61

--- tests/test_auto.py
+++ tests/test_auto.py
@@ -322,3 +322,28 @@ def test_maisdetalhes_claim():
     finally:
         driver.quit()
 
+def test_deletar_job():
+    selenium_url = os.getenv("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
+    driver = webdriver.Remote(
+        command_executor=selenium_url,
+        options=webdriver.FirefoxOptions()
+    )
+
+    try:
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+
+
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("Admin")
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys("admin123")
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME, "orangehrm-login-button"))).click()
+
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(@class, 'oxd-main-menu-item--name') and text()='Admin']"))).click()
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(@class, 'oxd-topbar-body-nav-tab-item') and text()='Job ']"))).click()
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(@class, 'oxd-topbar-body-nav-tab-link')and text()='Job Titles']"))).click()
+
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//i[contains(@class, 'oxd-icon bi-trash')]"))).click()
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//button[text()=' Yes, Delete ']"))).click()
+
+        WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, "//h6[contains(@class, 'oxd-text oxd-text--h6 orangehrm-main-title')]")))
+    finally:
+        driver.quit()
