c12b087e8c049770e61423c9aaea5e1fa8a50772
ReinaldoLuizSilva <reinaldodasilva@ienh.com.br>
2024-11-24 21:03:20 -0300

    teste53

--- tests/test_auto.py
+++ tests/test_auto.py
@@ -291,5 +291,11 @@ def criar_job():
         WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(@class, 'oxd-topbar-body-nav-tab-link')and text()='Job Titles']"))).click()
         WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'oxd-button--secondary') and text()=' Add ']"))).click()
 
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, "//span[contains(@class, 'oxd-input oxd-input--active'"))).send_keys("teste job")
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//button[text()=' Save ']"))).click()
+
+        WebDriverWait(driver, 2).until(EC.url_contains("dviewJobTitleList"))
+        assert "viewJobTitleList" in driver.current_url.lower()
+
     finally:
         driver.quit()
