a421a61e9ac8626025efdc56b8679cf981c708d2
ReinaldoLuizSilva <reinaldodasilva@ienh.com.br>
2024-11-24 21:14:34 -0300

    teste54

--- tests/test_auto.py
+++ tests/test_auto.py
@@ -299,3 +299,25 @@ def criar_job():
 
     finally:
         driver.quit()
+
+def mais_detalhes_claim():
+    selenium_url = os.getenv("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
+    driver = webdriver.Remote(
+        command_executor=selenium_url,
+        options=webdriver.FirefoxOptions()
+    )
+
+    try:
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+
+
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("Admin")
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys("admin123")
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME, "orangehrm-login-button"))).click()
+
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(@class, 'oxd-text oxd-text--span oxd-main-menu-item--name') and text()='Claim']"))).click()
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'oxd-button oxd-button--medium oxd-button--text oxd-table-cell-action-space') and text()=' View Details ']"))).click()
+
+        WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, "//h6[contains(@class, 'oxd-text oxd-text--h6 orangehrm-main-title')]")))
+    finally:
+        driver.quit()
